b9ecfe841ff05026aa57051e7e5a91c93f4f7a71
pranesh <42761086+praneshronaldo97@users.noreply.github.com>
2019-06-24 11:45:11 +0530

    This problem was asked by Twitter.
    Implement an auto-complete system. That is, given a query string s and a set of all possible query strings, return all strings in the set that have s as a prefix.
    For example, given the query string de and the set of strings [dog, deer, deal], return [deer, deal].
    Hint: Try preprocessing the dictionary into a more efficient data structure to speed up queries

---
+++ subb.java
@@ -0,0 +1,13 @@
+﻿import java.util.Scanner;
+public class subb {
+public static void main(String[] args) {
+Scanner s=new Scanner(System.in);
+String[] s1={"dog", "deer", "dealdog"};
+String s2=s.nextLine();
+for(int i=0;i<s1.length;i++) {
+if(s1[i].substring(0,s2.length()).equals(s2)) {
+System.out.println(s1[i]);
+}
+}
+}
+}
